2599ae1af06d020cc0e41f21b12836a8892458f4
neelkamath <neelkamathonline@gmail.com>
2021-04-30 17:04:27 +0530

    Add connected components tests

--- main.cpp
+++ main.cpp
@@ -21,5 +21,16 @@ int main() {
 
     cout << ds.same("one", "two") << '\n';
     cout << ds.same("one", "three") << '\n';
+
+    // Copy constructor demo.
+    auto copy1 = ds;
+    auto copy2 = disjoint_set(ds);
+    copy1.insert("five");
+    copy2.insert("six");
+    cout << '\n';
+    ds.disp_link();
+    copy1.disp_link();
+    copy2.disp_link();
+
     return 0;
 }
